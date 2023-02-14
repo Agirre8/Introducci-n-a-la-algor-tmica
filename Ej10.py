414ef885ef3fef7c378177bddb398bfbd50be2b3
Agirre8 <inagi30@gmail.com>
2023-02-14 13:08:02 +0100

    ej10

---
+++ Ej10.py
@@ -0,0 +1,20 @@
+Algoritmo: Área del triángulo
+
+Entrada:
+#Longitud del lado del triángulo
+l: REAL
+#Altura correspondiente al lado
+h: REAL
+
+Precondición:
+l > 0
+h > 0
+
+Realización:
+#Área del triángulo, calculada como la mitad del producto de la longitud del lado y su altura correspondiente
+Resultado <-- (l * h) / 2
+
+Poscondición:
+Resultado = (l * h) / 2
+
+Fin del cálculo del área del triángulo
